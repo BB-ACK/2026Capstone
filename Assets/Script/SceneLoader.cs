5fc60c770b9ae626590996626c10a9836a88f928
aincon <coran4178@gmail.com>
2026-01-02 22:23:57 +0900

    UI 제작
    Title, TheRoom, CharSeclect

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName); // 씬 이름으로 로드
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
